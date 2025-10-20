d440e112cd8de8c604a9e3ec06cf453a1c688ce0
tihomirtx88 <tihomirtx88@gmail.com>
2025-10-20 15:41:04 +0300

    Extends admin inteeface and add more models

--- job_aplication/admin.py
+++ job_aplication/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from .models import Form
+from .models import Form, Address
+
+@admin.action(description="Mark selected as active")
+def mark_as_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+class AddressInline(admin.TabularInline):
+    model = Address
+    extra = 1
 
 class FormAdmin(admin.ModelAdmin):
+   fieldsets = (
+       ("Personal Information", {
+            "fields": ("first_name", "last_name", "email")
+        }),
+        ("Job Details", {
+            "fields": ("occupation", "date"),
+            "classes": ("collapse",),  # collapsible section
+        }),
+   )
+
+   inlines = [AddressInline]
+
    list_display = ("first_name", "last_name", "email", "occupation", "date")
    list_display_links = ("first_name", "last_name");  # Which fields link to the edit page
    search_fields = ("first_name", "last_name", "email");
@@ -10,5 +30,8 @@ class FormAdmin(admin.ModelAdmin):
    readonly_fields = ("occupation",);
    list_editable = ("occupation",);
    list_per_page = 20;
+
+
+   actions = [mark_as_active]
 
 admin.site.register(Form, FormAdmin);
--- job_aplication/models.py
+++ job_aplication/models.py
@@ -9,3 +9,15 @@ class Form(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Address(models.Model):
+    # Link each address to a Form
+    form = models.ForeignKey(Form, related_name="addresses", on_delete=models.CASCADE)
+
+    street = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}"
