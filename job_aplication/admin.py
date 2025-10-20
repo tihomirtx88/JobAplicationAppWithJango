7280b20ae2a22ee280566caf254c6547a0ff3ccf
tihomirtx88 <tihomirtx88@gmail.com>
2025-10-20 15:22:25 +0300

    customizing admin interface

--- job_aplication/admin.py
+++ job_aplication/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form);
+class FormAdmin(admin.ModelAdmin):
+   list_display = ("first_name", "last_name", "email", "occupation", "date")
+   list_display_links = ("first_name", "last_name");  # Which fields link to the edit page
+   search_fields = ("first_name", "last_name", "email");
+   list_filter = ("date", "occupation");
+   ordering = ("first_name",);
+   readonly_fields = ("occupation",);
+   list_editable = ("occupation",);
+   list_per_page = 20;
+
+admin.site.register(Form, FormAdmin);
