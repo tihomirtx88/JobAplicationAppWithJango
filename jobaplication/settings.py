51b552ede13187878304ffe9b992476e4878ae6e
tihomirtx88 <tihomirtx88@gmail.com>
2025-10-21 15:58:41 +0300

    Adding navigation navbar

--- jobaplication/settings.py
+++ jobaplication/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'job_aplication'
+    'job_aplication',
+    'accounts'
 ]
 
 MIDDLEWARE = [
