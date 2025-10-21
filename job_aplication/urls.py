465d9e9fd0d5255e8458b601b570cc7ad1796e2e
tihomirtx88 <tihomirtx88@gmail.com>
2025-10-21 16:36:36 +0300

    Adding logut html and also refactor all app structure

--- job_aplication/urls.py
+++ job_aplication/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.aboutPage, name='about')
+    path('about/', views.aboutPage, name='about'),
+    path('accounts/', include('accounts.urls')),
 ];
